d6b520e37c50e9c7f4738d0b3eba7bf261612b6d
andresASIR <andres.narjim@gmail.com>
2022-02-13 14:32:21 +0100

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-app.use(express.static('./dist/formulaOne'));
+app.use(express.static('./dist/formula-one'));
 app.get('/*', function(req, res) {
- res.sendFile('index.html', {root: 'dist/formulaOne/'}
+ res.sendFile('index.html', {root: 'dist/formula-one/'}
  );
 });
 const port = 3500;
